21772b4a0ba6f3baed63213c38cf5d0cf86b3157
tady <a.dat.jp@gmail.com>
2014-04-05 14:58:17 +0900

    TwiML修正

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -7,7 +7,7 @@ class TwimlController < ApplicationController
   # Twilioの最初
   def start
     xml_str = Twilio::TwiML::Response.new do |response|
-      response.Say "バイ コール をご利用いただき ありがとう ございます。 ", :language => "ja-jp"
+      response.Say " バイ コール をご利用いただき ありがとう ございます。 ", :language => "ja-jp"
       response.Redirect "#{Setting.app_host}/twiml/ask_shop", method: 'GET'
     end.text
 
@@ -19,7 +19,7 @@ class TwimlController < ApplicationController
     # TwiMLを作成
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '*', action: "#{Setting.app_host}/twiml/ask_product", method: 'GET' do |gather|
-        gather.Say "ショップ アイディ を入力 してください。 入力が 完了したら アスタリスク を 押してください", :language => "ja-jp"
+        gather.Say " ショップ アイディ を入力 してください。 入力が 完了したら アスタリスク を 押してください", :language => "ja-jp"
       end
     end.text
 
@@ -30,7 +30,7 @@ class TwimlController < ApplicationController
     # TwiMLを作成
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '*', action: "#{Setting.app_host}/twiml/confirm_product", method: 'GET' do |gather|
-        gather.Say "商品 アイディ を入力 してください。 入力が 完了したら アスタリスク を 押してください", :language => "ja-jp"
+        gather.Say " 商品 アイディ を入力 してください。 入力が 完了したら アスタリスク を 押してください", :language => "ja-jp"
       end
     end.text
 
@@ -41,8 +41,8 @@ class TwimlController < ApplicationController
     # TwiMLを作成
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '', numDigits: 1, action: "#{Setting.app_host}/twiml/finish", method: 'GET' do |gather|
-        gather.Say "入力した商品 アイディは #{params[:Digits]} ですね。", :language => "ja-jp"
-        gather.Say "よろしければ、1を。修正したいかたは、9を押してください。", :language => "ja-jp"
+        gather.Say " 入力した商品 アイディは #{params[:Digits]} ですね。", :language => "ja-jp"
+        gather.Say " よろしければ、1を。 修正したいかたは、9を押してください。", :language => "ja-jp"
       end
     end.text
 
