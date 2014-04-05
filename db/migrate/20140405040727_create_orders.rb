e5f6ec7d8515bbe61e39850aea0f422bb0a67285
tady <a.dat.jp@gmail.com>
2014-04-05 19:30:53 +0900

    個数, 注文完了フラグ追加

--- db/migrate/20140405040727_create_orders.rb
+++ db/migrate/20140405040727_create_orders.rb
@@ -4,6 +4,7 @@ class CreateOrders < ActiveRecord::Migration
       t.integer :user_id   , null: false
       t.integer :product_id, null: false
       t.integer :count,      null: false, default: 1
+      t.boolean :confirm_finished, default: false
 
       t.timestamps
     end
