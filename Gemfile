2c302637c7e25edee482e50915c05c7c6989b763
kiiita <kitter2011@gmail.com>
2014-04-05 11:37:43 +0900

    bootstarp導入

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'therubyracer', platforms: :ruby
 gem 'jquery-rails'
 gem 'sass-rails', '~> 4.0.0'
 gem 'less-rails'
+gem 'bootstrap-sass', '3.0.2.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module Buycall
   class Application < Rails::Application
+    # for bootstrap-sass
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
