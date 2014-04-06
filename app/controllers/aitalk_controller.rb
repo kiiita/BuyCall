cbc33b716d496faa89079b5b18a70d8e608c24e8
tady <a.dat.jp@gmail.com>
2014-04-06 13:24:57 +0900

    add GA

--- app/controllers/aitalk_controller.rb
+++ app/controllers/aitalk_controller.rb
@@ -5,8 +5,6 @@ class AitalkController < ApplicationController
   protect_from_forgery with: :null_session
 
   def play
-    p params[:url]
-    p URI.parse(params[:url])
     send_data(Net::HTTP.get_response(URI.parse(params[:url])).body,
               filename: "voice.mp3",
               type: "audio/mpeg",
