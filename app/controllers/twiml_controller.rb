e4f6dd221cd4d74ea7c0d2dc6b330d640aa8f819
tady <tady@zigexn.co.jp>
2014-04-07 09:05:57 +0900

    AI Talkが不安定なため利用停止

--- app/controllers/twiml_controller.rb
+++ app/controllers/twiml_controller.rb
@@ -9,7 +9,8 @@ class TwimlController < ApplicationController
   # Twilioの最初
   def start
     xml_str = Twilio::TwiML::Response.new do |response|
-      response.Play voice_url('こちらは、バイコールです。 案内を開始します。')
+      # response.Play voice_url('こちらは、バイコールです。 案内を開始します。')
+      response.Say 'こちらは、バイコールです。 案内を開始します。', language: "ja-jp"
       response.Redirect "#{Setting.app_host}/twiml/ask_product", method: 'GET'
     end.text
 
@@ -19,7 +20,8 @@ class TwimlController < ApplicationController
   def ask_product
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '#', action: "#{Setting.app_host}/twiml/receive_product", method: 'GET' do |gather|
-        gather.Play voice_url("商品番号を入力してください。 最後に、シャープを押してください。")
+        # gather.Play voice_url("商品番号を入力してください。 最後に、シャープを押してください。")
+        gather.Say "商品番号を入力してください。 最後に、シャープを押してください。", language: "ja-jp"
       end
     end.text
 
@@ -30,7 +32,8 @@ class TwimlController < ApplicationController
     xml_str = Twilio::TwiML::Response.new do |response|
       # 該当する商品がない場合
       if params_pruduct.nil?
-        response.Play voice_url("入力した番号の商品がみつかりません。")
+        # response.Play voice_url("入力した番号の商品がみつかりません。")
+        response.Say "入力した番号の商品がみつかりません。", language: "ja-jp"
         response.Redirect "#{Setting.app_host}/twiml/ask_product", method: 'GET'
       else
         response.Redirect "#{Setting.app_host}/twiml/ask_count/#{params_pruduct.id}", method: 'GET'
@@ -48,7 +51,8 @@ class TwimlController < ApplicationController
   def ask_count
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '#', action: "#{Setting.app_host}/twiml/receive_product_and_count/#{params_pruduct.id}", method: 'GET' do |gather|
-        gather.Play voice_url("商品の個数を入力してください。最後に、シャープを押してください。")
+        # gather.Play voice_url("商品の個数を入力してください。最後に、シャープを押してください。")
+        gather.Say "商品の個数を入力してください。最後に、シャープを押してください。", language: "ja-jp"
       end
     end.text
 
@@ -58,8 +62,10 @@ class TwimlController < ApplicationController
   def receive_product_and_count
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '', numDigits: 1, action: "#{Setting.app_host}/twiml/confirm_product/#{params_pruduct.id}/#{params_count}", method: 'GET' do |gather|
-        gather.Play voice_url("ご注文は、#{params_pruduct.name_read}。個数は #{params_count}個、ですね。")
-        gather.Play voice_url("よろしければ、1を。修正したいかたは、9を押してください。")
+        # gather.Play voice_url("ご注文は、#{params_pruduct.name_read}。個数は #{params_count}個、ですね。")
+        # gather.Play voice_url("よろしければ、1を。修正したいかたは、9を押してください。")
+        gather.Say "ご注文は、#{params_pruduct.name_read}。個数は #{params_count}個、ですね。", language: "ja-jp"
+        gather.Say "よろしければ、1を。修正したいかたは、9を押してください。", language: "ja-jp"
       end
     end.text
 
@@ -89,7 +95,8 @@ class TwimlController < ApplicationController
 
   def ask_name
     xml_str = Twilio::TwiML::Response.new do |response|
-      response.Play voice_url("お名前をお話ください。最後に、シャープを押してください。")
+      # response.Play voice_url("お名前をお話ください。最後に、シャープを押してください。")
+      response.Say "お名前をお話ください。最後に、シャープを押してください。", language: "ja-jp"
       response.Record maxLength: '30', finishOnKey: '#', action: "#{Setting.app_host}/twiml/receive_name", :method => 'get'
     end.text
 
@@ -109,7 +116,8 @@ class TwimlController < ApplicationController
   def ask_zip
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '', numDigits: 7, action: "#{Setting.app_host}/twiml/receive_zip", method: 'GET' do |gather|
-        response.Play voice_url("郵便番号をハイフン無しで入力してください。")
+        # response.Play voice_url("郵便番号をハイフン無しで入力してください。")
+        response.Say "郵便番号をハイフン無しで入力してください。", language: "ja-jp"
       end
 
     end.text
@@ -122,19 +130,23 @@ class TwimlController < ApplicationController
 
       # 郵便番号が不正だったら
       if params[:Digits].blank? || params[:Digits].length != 7
-        response.Play voice_url("郵便番号に誤りがあります。")
+        # response.Play voice_url("郵便番号に誤りがあります。")
+        response.Say "郵便番号に誤りがあります。", language: "ja-jp"
         response.Redirect "#{Setting.app_host}/twiml/ask_zip", method: 'GET'
 
       elsif ZipCode.find_address_by_code(params[:Digits]).nil?
-        response.Play voice_url("入力した郵便番号の住所がみつかりません。")
+        # response.Play voice_url("入力した郵便番号の住所がみつかりません。")
+        response.Say "入力した郵便番号の住所がみつかりません。", language: "ja-jp"
         response.Redirect "#{Setting.app_host}/twiml/ask_zip", method: 'GET'
 
       else
         @current_caller_user.update(zip: params[:Digits], address1: ZipCode.find_address_by_code(params[:Digits]))
 
-        response.Play voice_url(" #{ZipCode.find_address_by_code(params[:Digits])} ですね。")
+        # response.Play voice_url(" #{ZipCode.find_address_by_code(params[:Digits])} ですね。")
+        response.Say " #{ZipCode.find_address_by_code(params[:Digits])} ですね。", language: "ja-jp"
         response.Gather timeout: 30, finishOnKey: '', numDigits: 1, action: "#{Setting.app_host}/twiml/confirm_zip", method: 'GET' do |gather|
-          gather.Play voice_url("よろしければ、1を。修正したいかたは、9を押してください。")
+          # gather.Play voice_url("よろしければ、1を。修正したいかたは、9を押してください。")
+          gather.Say "よろしければ、1を。修正したいかたは、9を押してください。", language: "ja-jp"
         end
       end
     end.text
@@ -157,7 +169,8 @@ class TwimlController < ApplicationController
   # 追加の住所
   def ask_address2
     xml_str = Twilio::TwiML::Response.new do |response|
-      response.Play voice_url("番地以降の住所をお話ください。最後に、シャープを押してください。")
+      # response.Play voice_url("番地以降の住所をお話ください。最後に、シャープを押してください。")
+      response.Say "番地以降の住所をお話ください。最後に、シャープを押してください。", language: "ja-jp"
       response.Record maxLength: '60', finishOnKey: '#', action: "#{Setting.app_host}/twiml/receive_address2", :method => 'get'
     end.text
 
@@ -179,9 +192,12 @@ class TwimlController < ApplicationController
     sum = @current_caller_user.orders.last.count * @current_caller_user.orders.last.product.price
     xml_str = Twilio::TwiML::Response.new do |response|
       response.Gather timeout: 30, finishOnKey: '', numDigits: 1, action: "#{Setting.app_host}/twiml/confirm_order", method: 'GET' do |gather|
-        gather.Play voice_url("ご注文を確認いたします。")
-        gather.Play voice_url("ご注文は、#{@current_caller_user.orders.last.product.name_read}。個数は #{@current_caller_user.orders.last.count} 個　。　お会計は、#{sum}円、になります。 ")
-        gather.Play voice_url("よろしければ、1を。 修正したいかたは、9を押してください。")
+        # gather.Play voice_url("ご注文を確認いたします。")
+        # gather.Play voice_url("ご注文は、#{@current_caller_user.orders.last.product.name_read}。個数は #{@current_caller_user.orders.last.count} 個　。　お会計は、#{sum}円、になります。 ")
+        # gather.Play voice_url("よろしければ、1を。 修正したいかたは、9を押してください。")
+        gather.Say "ご注文を確認いたします。", language: "ja-jp"
+        gather.Say "ご注文は、#{@current_caller_user.orders.last.product.name_read}。個数は #{@current_caller_user.orders.last.count} 個　。　お会計は、#{sum}円、になります。 ", language: "ja-jp"
+        gather.Say "よろしければ、1を。 修正したいかたは、9を押してください。", language: "ja-jp"
       end
     end.text
 
@@ -204,7 +220,8 @@ class TwimlController < ApplicationController
     @current_caller_user.orders.last.update(confirm_finished: true)
 
     xml_str = Twilio::TwiML::Response.new do |response|
-      response.Play voice_url("ご注文ありがとうございました。 後ほどショップから確認の電話をすることがあります。")
+      # response.Play voice_url("ご注文ありがとうございました。 後ほどショップから確認の電話をすることがあります。")
+      response.Say "ご注文ありがとうございました。 後ほどショップから確認の電話をすることがあります。", language: "ja-jp"
     end.text
 
     render xml: xml_str
